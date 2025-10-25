b12cd5d53abb779f6a8f8e7f31fab90ec1a81e21
gimhanijayamanna <pabasarajayamanna@gmail.com>
2025-10-26 00:23:43 +0530

    add why choose us component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthContainer from "./components/auth/AuthContainer";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<AuthContainer />} />
         <Route path="/signup" element={<AuthContainer />} />
       </Routes>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import WhyChooseUs from "../components/WhyChooseUs";
+
+export default function Home() {
+    return (
+        <main>
+            {/* Other landing hero or banner could go here */}
+            <WhyChooseUs />
+        </main>
+    );
+}
