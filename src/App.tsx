d765ec9b12d224ed7c82ef7acee03bf84c539b66
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-10-26 12:10:43 +0530

    feat: add Leadership Team section with LeadershipCard component and lucide-react dependency

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        
       </Routes>
     </Router>
   );
