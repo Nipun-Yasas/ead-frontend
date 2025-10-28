8cd5288cebdbcd04ef5fd017f4065b65b2a61a07
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2025-10-28 23:20:13 +0530

    updated hero

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Hero from "./components/Hero";
-import Cetificate from "./components/Cetificate";
-import Experiance from "./components/Experiance";
+
 
 function App() {
   return (
@@ -13,8 +12,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/hero" element={<Hero/>}/>
-        <Route path="/cetificate" element={<Cetificate/>}/>
-        <Route path="/experiance" element={<Experiance/>}/>
+  
       </Routes>
     </Router>
   );
