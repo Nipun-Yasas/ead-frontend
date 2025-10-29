789fb01843814c18ccc0aa822229c6054be4319f
gimhanijayamanna <pabasarajayamanna@gmail.com>
2025-10-29 11:20:03 +0530

    removed bookAppoinment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthContainer from "./components/auth/AuthContainer";
-import BookingAppointment from "./components/BookingAppointment";
 import WhyChooseUs from "./components/WhyChooseUs";
 
 export default function App() {
@@ -9,10 +8,8 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<AuthContainer />} />
-        <Route path="/book" element={<BookingAppointment />} />
         <Route path="/signup" element={<AuthContainer />} />
         <Route path="/whyChooseUs" element={<WhyChooseUs />} />
-
       </Routes>
     </Router>
   );
