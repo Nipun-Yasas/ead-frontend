2c8c5b38713597d00f8c9b478ddf7c795452fdce
Nipun Yasas <bandaraopnyk.22@uom.lk>
2025-11-07 10:12:46 +0530

    feat: added employee dashboard and it's features.

--- src/utils/employeeNavigation.tsx
+++ src/utils/employeeNavigation.tsx
@@ -13,22 +13,6 @@ export const employeeNavigation: Navigation = [
     icon: <DashboardIcon />,
   },
   {
-    segment: 'employee/products',
-    title: 'My Products',
-    icon: <InventoryIcon />,
-  },
-  {
-    segment: 'employee/orders',
-    title: 'Orders',
-    icon: <ShoppingCartIcon />,
-  },
-  {
-    segment: 'employee/reports',
-    title: 'Reports',
-    icon: <AssessmentIcon />,
-  },
-  //navigation for the chat
-  {
     segment: 'employee/messages',
     title: 'Messages',
     icon: <ChatIcon />,
